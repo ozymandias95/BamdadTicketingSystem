caf7bfa000b46514c3fc851989138eddc37c7328
Ozymandias95 <-help>
2024-01-23 20:11:01 +0330

    Creating the initial project architecture and folders based on needs Choose mvc as user interface Create Admin and User Area

---
+++ TicketingSystemProject/App.EndPoints.MVC/Areas/UserArea/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoints.MVC.Areas.UserArea.Controllers;
+
+[Area("User")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
